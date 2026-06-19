aa43d362265cb4338baf321c264b1e19b251c9ca
hikhjin <hikhjin@gmail.com>
2026-06-19 14:43:33 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/kotlin/com/youthexpedition/azit/batch/AzitBatchApplication.kt
@@ -0,0 +1,11 @@
+package com.youthexpedition.azit.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AzitBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<AzitBatchApplication>(*args)
+}
